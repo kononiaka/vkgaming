438e0f455e0a01390350fdfc5515b98d770b16b6
kononiaka <kononiaka.vladimir@gmail.com>
2023-06-09 22:25:47 +0200

    tournament bracket edited

--- src/components/tournaments/homm3/Tournaments.js
+++ src/components/tournaments/homm3/Tournaments.js
@@ -2,64 +2,55 @@ import React, { useContext, useEffect, useState } from 'react';
 import { lookForUserId } from '../../../api/api';
 import AuthContext from '../../../store/auth-context';
 import classes from './Tournaments.module.css';
+import { TournamentBracket, renderPlayerList } from './tournamentsBracket';
 
 const TournamentList = () => {
     const [tournaments, setTournaments] = useState([]);
     const [tournamentId, setTournamentId] = useState([]);
+    const [showDetails, setShowDetails] = useState(false);
     const authCtx = useContext(AuthContext);
     let { userNickName } = authCtx;
 
-    useEffect(() => {
-        const fetchTournaments = async () => {
-            try {
-                const response = await fetch(
-                    'https://test-prod-app-81915-default-rtdb.firebaseio.com/tournaments/heroes3.json'
-                );
-                const data = await response.json();
-
-                if (response.ok) {
-                    const tournamentList = Object.keys(data)
-                        .map((key) => {
-                            const tournament = data[key];
-                            if (tournament) {
-                                setTournamentId(tournament.id);
-                            }
-                            return tournament ? { id: key, ...tournament } : null;
-                        })
-                        .filter(Boolean);
-
-                    setTournaments(tournamentList);
-                } else {
-                    console.error('Failed to fetch tournaments:', data);
+    const fetchTournaments = async () => {
+        try {
+            const response = await fetch(
+                'https://test-prod-app-81915-default-rtdb.firebaseio.com/tournaments/heroes3.json'
+            );
+            const data = await response.json();
+            if (response.ok) {
+                const tournamentList = Object.keys(data)
+                    .map((key) => {
+                        const tournament = data[key];
+                        return tournament ? { id: key, ...tournament } : null;
+                    })
+                    .filter(Boolean);
+                setTournaments(tournamentList);
+                // Update tournament ID outside the map function
+                if (tournamentList.length > 0) {
+                    setTournamentId(tournamentList[0].id);
                 }
-            } catch (error) {
-                console.error('Error fetching tournaments:', error);
+            } else {
+                console.error('Failed to fetch tournaments:', data);
             }
-        };
+        } catch (error) {
+            console.error('Error fetching tournaments:', error);
+        }
+    };
 
+    useEffect(() => {
         fetchTournaments();
     }, []);
 
-    const formatPlayerName = (player) => player.name;
-
     const checkRegisterUser = (currentUser, players) => {
-        Object.values(players)
-            .filter((player) => player !== null) // Filter out null values
-            .map((player) => {
-                if (player.name === currentUser) {
-                    console.log('true');
-                    return true;
-                } else {
-                    console.log('false');
-                    return false;
-                }
-            });
+        const registeredPlayers = Object.values(players).filter((player) => player !== null);
+        return registeredPlayers.some((player) => player.name === currentUser);
     };
+
     const addUserTournament = async (tourId, nickname) => {
         const user = await lookForUserId(nickname, 'full');
 
-        console.log('user', user);
-        console.log('tourId', tourId);
+        // console.log('user', user);
+        // console.log('tourId', tourId);
 
         const response = await fetch(
             `https://test-prod-app-81915-default-rtdb.firebaseio.com/tournaments/heroes3/${tourId}/players/.json`,
@@ -71,33 +62,45 @@ const TournamentList = () => {
                 }
             }
         );
+
+        if (response.ok) {
+            // Reload the page if the response is successful
+            window.location.reload();
+        }
     };
 
-    // TournamentBracket();
-    // checkRegisterUser(authCtx);
+    const showDetailsHandler = () => {
+        setShowDetails((prevState) => !prevState);
+    };
 
+    let currentPlayers;
     const tournamentList =
         tournaments.length > 0 ? (
             <ul>
                 {tournaments.map((tournament) => {
-                    console.log('tournament', tournament);
+                    // maxPlayers = tournament.maxPlayers;
+                    currentPlayers = tournament.players;
+                    // console.log('tournament', tournament);
                     // console.log('players in tournament ?', 'players' in tournament);
-
                     return (
                         <li key={tournament.id} className={classes.bracket}>
                             <h3>{tournament.name}</h3>
                             <p>Status: {tournament.status}</p>
                             <p>
-                                Players registered:
+                                Players registered:&nbsp;
                                 {'players' in tournament &&
                                     Object.values(tournament.players).filter((player) => player !== null).length}
                             </p>
                             <p>Max players: {tournament.maxPlayers}</p>
-                            <h4>Players:</h4>
+
+                            <button onClick={() => showDetailsHandler()}>View details</button>
                             {'players' in tournament &&
                             Object.keys(tournament.players).length < tournament.maxPlayers ? (
                                 checkRegisterUser(userNickName, tournament.players) ? (
-                                    <p>You are registered!</p>
+                                    <div>
+                                        {/* TODO make a toggle */}
+                                        <p>You are already registered!</p>
+                                    </div>
                                 ) : (
                                     <button onClick={() => addUserTournament(tournamentId, userNickName)}>
                                         Register
@@ -107,13 +110,7 @@ const TournamentList = () => {
                                 <p>Registration finished!</p>
                             )}
                             {'players' in tournament ? (
-                                <ul>
-                                    {Object.values(tournament.players)
-                                        .filter((player) => player !== null)
-                                        .map((player) => (
-                                            <li key={player.name}>{formatPlayerName(player)}</li>
-                                        ))}
-                                </ul>
+                                showDetails && renderPlayerList(tournament.players)
                             ) : (
                                 <>
                                     <p>No players registered.</p>
@@ -136,6 +133,7 @@ const TournamentList = () => {
             <h2>Current Tournaments</h2>
             {/* <h2>Future Tournaments</h2> */}
             {tournamentList}
+            {showDetails && <TournamentBracket maxPlayers={currentPlayers}></TournamentBracket>}
         </div>
     );
 };
--- src/components/tournaments/homm3/tournamentsBracket.js
+++ src/components/tournaments/homm3/tournamentsBracket.js
@@ -1,33 +1,75 @@
-// export const TournamentBracket = (maxPlayers) => {
-//     // const maxPlayers = 8; // Maximum number of players
-//     const stages = ['1/4', '1/2', 'Third Place', 'Final', 'Winner']; // Bracket stages
+import classes from './tournamentsBracket.module.css';
 
-//     // Determine the number of matches and teams for each stage
-//     const matchesPerStage = [maxPlayers / 2, maxPlayers / 4, 2, 1];
-//     const teamsPerMatch = [2, 2, 2, 2];
+const formatPlayerName = (player) => player.name;
 
-//     // console.log(stages);
+export const renderPlayerList = (players) => (
+    <>
+        <h4>Players:</h4>
+        <ul>
+            {Object.values(players)
+                .filter((player) => player !== null)
+                .map((player) => (
+                    <li key={player.name}>{formatPlayerName(player)}</li>
+                ))}
+        </ul>
+    </>
+);
 
-//     // Create an array of matches for each stage
-//     const brackets = stages.map((stage, index) => {
-//         const numMatches = matchesPerStage[index];
-//         const numTeams = teamsPerMatch[index];
+export const TournamentBracket = ({ maxPlayers }) => {
+    // console.log('maxPlayers', maxPlayers);
+    maxPlayers.length = Object.keys(maxPlayers).length;
+    let stageLabels = ['1/8 Final', 'Quater-final', 'Semi-final', 'Third Place', 'Final', 'Winner'];
 
-//         return Array.from({ length: numMatches }, (_, matchIndex) => ({
-//             stage,
-//             matchIndex,
-//             teams: Array.from({ length: numTeams }, (__, teamIndex) => ({
-//                 playerIndex: matchIndex * numTeams + teamIndex
-//                 // name: players[matchIndex * numTeams + teamIndex]?.name || 'TBD'
-//             }))
-//         }));
-//     });
-// };
+    // Determine the stage label based on the number of max players
+    if (maxPlayers && maxPlayers.length === 4) {
+        stageLabels = ['Semi-final', 'Third Place', 'Final', 'Winner'];
+    } else if (maxPlayers && maxPlayers.length === 8) {
+        stageLabels = ['Quater-final', 'Semi-final', 'Third Place', 'Final', 'Winner'];
+    } else if (maxPlayers && maxPlayers.length === 16) {
+        stageLabels = ['1/8 Final', 'Quater-final', 'Semi-final', 'Third Place', 'Final', 'Winner'];
+    }
 
-// export const checkRegisterUser = (authCtx, players) => {
-//     console.log('players', players);
-//     let { userNickName } = authCtx;
-//     console.log(userNickName);
+    const playoffPairs = [
+        // Playoff pairs for each stage
+        [
+            { team1: 'Team A', team2: 'Team B' },
+            { team1: 'Team C', team2: 'Team D' }
+            // Add more playoff pairs for the 1/8 Final stage
+        ],
+        [
+            { team1: 'Team E', team2: 'Team F' },
+            { team1: 'Team G', team2: 'Team H' }
+            // Add more playoff pairs for the Quarter-final stage
+        ]
+        // Add playoff pairs for the remaining stages
+    ];
 
-//     return true;
-// };
+    return (
+        <div>
+            {stageLabels.map((stage, index) => {
+                // console.log('Current stage:', stage); // Add the console log statement here
+                console.log('playoffPairs[index]', playoffPairs[index]);
+                return (
+                    <div key={stage} className={classes.brackets}>
+                        <h3>{stage}</h3>
+                        {playoffPairs[index]?.map((pair, pairIndex) => (
+                            <div key={pairIndex}>
+                                <p>{`Match ${pairIndex + 1}`}</p>
+                                <p>{pair.team1}</p>
+                                <p>{pair.team2}</p>
+                            </div>
+                        ))}
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
+
+export const checkRegisterUser = (authCtx, players) => {
+    console.log('players', players);
+    let { userNickName } = authCtx;
+    console.log(userNickName);
+
+    return true;
+};
